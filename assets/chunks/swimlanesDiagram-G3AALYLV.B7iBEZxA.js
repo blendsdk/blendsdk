582a9e98a8c0692c935b5f853f2b96ee0b2f0ccd
TrueSoftwareNL_truesoftware_runner_1 <TrueSoftwareNL_truesoftware_runner_1@truesoftware.nl>
2026-09-03 17:43:35 +0000

    docs: deploy v5.53.0 documentation site

---
+++ assets/chunks/swimlanesDiagram-G3AALYLV.B7iBEZxA.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.BgCC1F3L.js";import{_ as a}from"../app.EFw679vR.js";import"./chunk-5VM5RSS4.DN6KbW2u.js";import"./chunk-XXDRQBXY.XFU3pkL4.js";import"./chunk-VR4S4FIN.Dy7lmv-1.js";import"./chunk-32BRIVSS.CRnK6_pr.js";import"./channel.DpRZ8Fjt.js";import"./framework.C9jfXJBR.js";import"./theme.gTlaFdrK.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
